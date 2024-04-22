42bce505db834d56acee51159e8663aceff18322
saraGasperini70 <gasperinisara79@gmail.com>
2024-04-22 09:53:05 +0200

    ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int	i = 1;
+	int	j = 0;
+
+	if (ac >= 2)
+	{
+		for (i = 1; av[i]; i++)
+		{
+			for (j = 0; av[i][j]; j++)
+				std::cout << (char) toupper(av[i][j]);
+			if (i < ac - 1)
+				std::cout << ' ';
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
