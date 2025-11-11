ed3aa5649594fe726ef9cd50df27ae13447bf743
Sara Gasperini <sgasperi@e3r8p3.42roma.it>
2025-11-11 14:03:42 +0100

    Cleaned up 09

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,27 +1,3 @@
-#include "BitcoinExchange.hpp"
-
-int main(int argc, char **argv) {
-	if (argc != 2) {
-		std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
-		return 1;
-	}
-
-	try {
-		BitcoinExchange exchange;
-		exchange.execProgram(argv[1]);
-	} catch (const BitcoinExchange::FileNotFoundException &e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
-	} catch (const BitcoinExchange::EmptyFileException &e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
-	} catch (const BitcoinExchange::InvalidDateException &e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
-	} catch (const BitcoinExchange::InvalidValueException &e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
-	}
-
-	return 0;
+int	main () {
+	
 }
