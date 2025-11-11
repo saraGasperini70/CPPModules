c6a9b2157fae6120ba9970301712312cf14215fb
Sara Gasperini <sgasperi@e3r10p3.42roma.it>
2025-11-11 17:14:40 +0100

    Cleaned CPP09

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -1,55 +1,21 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+
 #include <map>
+#include <string>
 
 class BitcoinExchange {
-	private:
-		std::map<std::string, float> _data;
-		void _loadData(const std::string &filename);
-		bool _isValidDate(const std::string &date) const;
-		double _parseValue(const std::string &value) const;
-		void _multiplyValue(std::string &value, double multiplier) const;
-		void _clearData();
-		public:
-		/*Constructors and deconstructor*/
-		BitcoinExchange();
-		~BitcoinExchange();
-		BitcoinExchange(const BitcoinExchange &other);
-		BitcoinExchange &operator=(const BitcoinExchange &other);
-
-		/*Methods*/
-		std::map<std::string, float> getData() const;
-		void execProgram(const std::string &filename);
-
-		/*Exceptions*/
-		class InvalidDateException : public std::exception {
-			public:
-				const char *what() const throw() {
-					return "Invalid date format";
-				}
-		};
-		class InvalidValueException : public std::exception {
-			public:
-				const char *what() const throw() {
-					return "Invalid value format";
-				}
-		};
-		class FileNotFoundException : public std::exception {
-			public:
-				const char *what() const throw() {
-					return "File not found";
-				}
-		};
-		class EmptyFileException : public std::exception {
-			public:
-				const char *what() const throw() {
-					return "File is empty";
-				}
-		};
+    private:
+        std::map<std::string, float> exchange_rate;
+        std::map<std::string, float> multiplier;
+    public:
+        BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange& src);
+        BitcoinExchange(std::map<std::string, float> multiplier);
+        ~BitcoinExchange();
+        BitcoinExchange BitcoinExchange& operator=(const BitcoinExchange& src);
+        void setExchangeRate(std::string exchange);
+        void setMultipliers(std::string multi);
 };
 
 #endif
